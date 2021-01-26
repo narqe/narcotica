3221e1c135d2a404900fda401d69bd0506589b57
joelacef <joelacef@gmail.com>
2021-01-26 17:26:57 -0300

    adding packages

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,16 +2,18 @@ import React from 'react'
 import { useStaticQuery, graphql } from "gatsby"
 import { Link } from "gatsby"
 
-const Navbar = () => {
-    const info = useStaticQuery(graphql`query getAllPages {
-            allSitePage(sort: {order: ASC, fields: id}, filter: {}) {
-                nodes {
-                    path,
-                    internalComponentName
-                }
-            }
-        } 
+const Navbar = ({some}) => {
+    const info = useStaticQuery(graphql`
+    query getAllPages {
+        allSitePage(sort: {order: ASC, fields: id}, filter: {}) {
+        nodes {
+            path,
+            internalComponentName
+        }
+        }
+    } 
     `)
+
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,7 +22,7 @@ const Navbar = () => {
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     {info.allSitePage.nodes.map(
-                        el => <Link className="nav-item nav-link" to={el.path}>{ el.internalComponentName.slice(9)}</Link>
+                        el => <Link className="nav-item nav-link" to={el.path}>{ el.internalComponentName.slice(9) || 'NoName'}</Link>
                     )}
                 </div>
             </div>
